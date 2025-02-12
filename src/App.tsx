179a2d7171f19e612dae8b51e7930aedc3fac77f
beasb <bia4sb4@gmail.com>
2025-02-12 15:22:28 -0300

    react + tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css'
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import Cadastro from "./pages/cadastro/Cadastro"
+import Login from "./pages/login/Login"
 
 function App() {
   return (
-    <Home />
+
+    <>
+         <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  
   )
 }
 
