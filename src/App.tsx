64a61af0921c6c7c171b121684e955d14f128735
beasb <bia4sb4@gmail.com>
2025-02-13 17:01:38 -0300

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,35 @@ import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
 import Cadastro from "./pages/cadastro/Cadastro"
 import Login from "./pages/login/Login"
+import { AuthProvider } from "./contexts/AuthContext"
 
 function App() {
   return (
 
     <>
+        <AuthProvider>
+         {/* Habilita o processo de rotas - como um satelite */}
          <BrowserRouter>
+
           <Navbar />
+
           <div className="min-h-[80vh]">
-            <Routes>
+            {/* Routes observa a url - pega a rota - funciona como o gps - faz a listagem das rotas*/}
+            <Routes>       
+              {/* a rua - destino final */}
               <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/" element={<Cadastro />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+
             </Routes>
+
           </div>
+
           <Footer />
+
         </BrowserRouter>
+        </AuthProvider>
     </>
   
   )
